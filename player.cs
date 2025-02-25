6d509fd3d4fdf82fe490a96e22aca298169197f6
ElizAlgo <elizavetaalgoritmika24@gmail.com>
2025-02-25 19:53:19 +0300

    Update player.cs

--- player.cs
+++ player.cs
@@ -1,1 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Player : MonoBehaviour
+{
+    public GameObject fireballPrefab;
+
+    public Transform attackPoint;
+
+    private int health = 10;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока" + health);
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab,attackPoint.position, attackPoint.rotation);
+        }
+    }
+}
